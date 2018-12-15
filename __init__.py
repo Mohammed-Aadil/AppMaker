12b577997ed72893587ace08f022f1419292feb5
codewithshahnawaz <shahnawaz28april@gmail.com>
2018-12-15 10:00:31 +0530

    fisrt commit

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from tkinter import*
+class Maker:
+    def __init__(self, master = Tk()):
+        self.master = master
+        self.master.title('AppMaker')
+    def run(self):
+        self.master.mainloop()
+class SetBtn:
+    def __init__(self,master,text='',code = None,row = None, col = None):
+        self.master = None
+        self.text = text
+        self.code = code
+        self.row = row
+        self.col = col
+        Button(self.master, text=self.text,command=self.code).grid(row=self.row, column=self.col)
+app = Maker()
+b1 = SetBtn(app,'run')
+app.run()
