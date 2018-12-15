8304f5eee5a22277e5902e2af1bcffe9828b75fa
codewithshahnawaz <shahnawaz28april@gmail.com>
2018-12-15 13:56:17 +0530

    commit

--- __init__.py
+++ __init__.py
@@ -4,19 +4,17 @@ import sys
 def press():
     print('Working')
 class Maker:
-    def __init__(self, master = Tk()):
-        self.master = master
+    def __init__(self):
+        self.master = Tk()
         self.master.title('AppMaker')
     def run(self):
         self.master.mainloop()
-class SetBtn:
-    def __init__(self,master,text='',code = None,row = None, col = None):
-        self.master = None
+    def SetBtn(self, text = None , command = None, row = None , col = None):
         self.text = text
-        self.code = code
+        self.command = command
         self.row = row
         self.col = col
-        Button(self.master, text=self.text,command=self.code).grid(row=self.row, column=self.col)
-app = Maker()
-b1 = SetBtn(app,'run',press)
-app.run()
+        Button(self.master, text=self.text, command = self.command).grid(row = self.row, column = self.col)
+    def Caption(self,title = 'AppMaker'):
+        self.title = title
+        self.master.title(self.title)
